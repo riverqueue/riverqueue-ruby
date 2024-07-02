cd8960813fe00781a022d2c0af8e037cc0ab87ee
Brandur <brandur@brandur.org>
2024-07-01 19:16:23 -0700

    Small aesthetic improvement to the FNV implementation
    An improvement to the FNV implementation that calculates a bitmask using
    a power of two minus one instead of hardcoding them.
    
        2^32-1 == 0xffffffff

--- lib/fnv.rb
+++ lib/fnv.rb
@@ -8,23 +8,17 @@ module River
   module FNV
     def self.fnv1_hash(str, size:)
       hash = OFFSET_BASIS.fetch(size)
-      mask = MASK.fetch(size)
+      mask = (2**size - 1).to_int # creates a mask of 1s of `size` bits long like 0xffffffff
       prime = PRIME.fetch(size)
 
       str.each_byte do |byte|
         hash *= prime
-        hash &= mask # take lower N bits of multiplication product
+        hash &= mask
         hash ^= byte
       end
 
       hash
     end
-
-    MASK = {
-      32 => 0xffffffff, # mask 32 bits long
-      64 => 0xffffffffffffffff # mask 64 bits long
-    }.freeze
-    private_constant :MASK
 
     OFFSET_BASIS = {
       32 => 0x811c9dc5,
